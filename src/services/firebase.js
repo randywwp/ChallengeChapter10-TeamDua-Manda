f3234f607d676b7899d84fe8f92f5f54ce9863ae
unknown <ahmaddrizkyyy@gmail.com>
2022-07-25 19:47:06 +0700

    Change Register and Firebase

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -1,6 +1,25 @@
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
+import {
+  GoogleAuthProvider,
+  getAuth,
+  signInWithPopup,
+  signInWithEmailAndPassword,
+  createUserWithEmailAndPassword,
+  sendPasswordResetEmail,
+  signOut
+} from 'firebase/auth'
+
+import {
+  getFirestore,
+  query,
+  getDocs,
+  doc,
+  collection,
+  where,
+  setDoc,
+  updateDoc,
+  } from 'firebase/firestore'
+
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
@@ -12,7 +31,93 @@ const firebaseConfig = {
   appId: "1:546197271681:web:95f89416f610c796254980",
 };
 
-const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
-export const db = getFirestore(app);
-export const storage = getStorage(app);
+
+const app = initializeApp(firebaseConfig)
+const auth = getAuth(app)
+const db = getFirestore(app)
+const storage = getStorage(app)
+
+
+const googleProvider = new GoogleAuthProvider()
+
+async function signInWithGoogle () {
+  try {
+    const res = await signInWithPopup(auth, googleProvider)
+    const user = res.user
+    const q = query(collection(db, 'users'), where('uid', '==', user.uid))
+    const docs = await getDocs(q)
+    if (docs.docs.length === 0) {
+      await setDoc(doc(db, 'users', user.uid), {
+        uid: user.uid,
+        name: user.displayName,
+        authProvider: 'google',
+        email: user.email
+      })
+    }
+  } catch (err) {
+    console.error(err)
+    alert(err.message)
+  }
+}
+
+async function logInWithEmailAndPassword (email, password) {
+  try {
+    const user = await signInWithEmailAndPassword(auth, email, password)
+    return user
+  } catch (err) {
+    console.error(err)
+    alert(err.message)
+  }
+}
+
+async function registerWithEmailAndPassword (name, email, password) {
+  try {
+    const res = await createUserWithEmailAndPassword(auth, email, password)
+    const user = res.user
+    await setDoc(doc(db, 'users', user.uid), {
+      uid: user.uid,
+      name,
+      authProvider: 'local',
+      email
+    })
+  } catch (err) {
+    console.error(err)
+    alert(err.message)
+  }
+}
+
+async function sendPasswordReset (email) {
+  try {
+    await sendPasswordResetEmail(auth, email)
+    alert('Password reset link sent!')
+  } catch (err) {
+    console.error(err)
+    alert(err.message)
+  }
+}
+
+
+async function updatePhotoProfile (uid, downloadUrl) {
+  // Cari data dari collection users yang mempunyai dokument sama dgn uid
+  // update dengan profile url-nya
+  await updateDoc(doc(db, 'users', uid), {
+    profileUrl: downloadUrl
+  })
+}
+
+function logout () {
+  signOut(auth)
+};
+
+
+export {
+  auth,
+  db,
+  logout,
+  sendPasswordReset,
+  registerWithEmailAndPassword,
+  logInWithEmailAndPassword,
+  signInWithGoogle,
+  storage,
+  updatePhotoProfile,
+  }
