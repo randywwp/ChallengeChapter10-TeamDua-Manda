15f3f4d11d7d827e39d650655b2c009f90cc1855
Andi Alfi Syahrin <andi.alfi.syahrin@mail.ugm.ac.id>
2022-08-17 20:57:14 +0800

    last commit andi

--- src/pages/register.jsx
+++ src/pages/register.jsx
@@ -23,7 +23,7 @@ export default function Register() {
 
     useEffect(() => {
         if(currentUser) {
-            router.push("/dashboard")
+            router.push("/")
         }
     }, [currentUser, router])
 
